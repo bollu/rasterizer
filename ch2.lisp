d72a267d299233337cee2227fb6783af25ec6eaf
Siddharth Bhat <siddu.druid@gmail.com>
2021-05-10 11:06:44 +0530

    draw a triangle using barycentric coordinates

--- ch2.lisp
+++ ch2.lisp
@@ -10,6 +10,10 @@
 ;; SLIME: https://www.youtube.com/watch?v=_B_4vhsmRRI
 ;; https://ambrevar.xyz/modern-common-lisp/
 ;; to change package, use ", set package"
+
+;; Destructuring bind:
+;; http://www.lispworks.com/documentation/HyperSpec/Body/m_destru.htm
+
 (declaim (optimize (speed 0) (safety 3) (debug 3)))
 (defpackage :ch2 (:use :common-lisp))
 ;; for uiop
@@ -19,15 +23,15 @@
 (defparameter +w+ 500)
 (defparameter +h+ 500)
 (defparameter +r+ 100)
-(defparameter +c+ (vector (/ +w+ 2) (/ +h+ 2)))
-(defun v3 (x y z) (vector x y z))
+(defparameter +c+ (list (/ +w+ 2) (/ +h+ 2)))
+(defun v3 (x y z) (list x y z))
 (defun v3->x (v) (elt v 0))
 (defun v3->y (v) (elt v 1))
 (defun v3->z (v) (elt v 2))
 
 
 ;;  check that this is indeed correct
-(defun v3x (p q)
+(defun v3cross (p q)
   (defun component (f g c d)
     (-  (*  (funcall f c) (funcall g d))
 	(* (funcall g c) (funcall f d))))
@@ -35,19 +39,19 @@
       (component #'v3->z #'v3->x p q)
       (component #'v3->x #'v3->y p q)))
 
-(v3x (v3 1 0 0) (v3 1 0 0))
-(v3x (v3 1 0 0) (v3 0 1 0))
-(v3x (v3 1 0 0) (v3 0 0 1))
+(v3cross (v3 1 0 0) (v3 1 0 0))
+(v3cross (v3 1 0 0) (v3 0 1 0))
+(v3cross (v3 1 0 0) (v3 0 0 1))
 
-(v3x (v3 0 1 0) (v3 1 0 0))
-(v3x (v3 0 1 0) (v3 0 1 0))
-(v3x (v3 0 1 0) (v3 0 0 1))
+(v3cross (v3 0 1 0) (v3 1 0 0))
+(v3cross (v3 0 1 0) (v3 0 1 0))
+(v3cross (v3 0 1 0) (v3 0 0 1))
 
-(v3x (v3 0 0 1) (v3 1 0 0))
-(v3x (v3 0 0 1) (v3 0 1 0))
-(v3x (v3 0 0 1) (v3 0 0 1))
+(v3cross (v3 0 0 1) (v3 1 0 0))
+(v3cross (v3 0 0 1) (v3 0 1 0))
+(v3cross (v3 0 0 1) (v3 0 0 1))
 
-(defun v2 (x y) (vector x y))
+(defun v2 (x y) (list x y))
 
 (defun v2->x (v)
   (elt v 0))
@@ -71,6 +75,12 @@
 (defun v2floor (v)
   (v2 (floor (v2->x v)) (floor (v2->y v))))
 
+;; P = aA + bB + cC
+;; a + b + c = 1 from geometry
+;; P = (1 - b - c) A + bC + cC
+;; 
+;; P = (1 - u - v) A + uB + vC
+;; P = A + u(B - A) + v(C - A)
 ;; P = A + uAB + vAC
 ;; 0 = (A - P) + uAB + vAC
 ;; 0 = PA + uAB + vAC
@@ -79,8 +89,9 @@
 ;; ------------------
 ;; 0 = (PAx, ABx, BCx) . (u, v, 1)
 ;; 0 = (PAy, ABy, BCy) . (u, v, 1)
-
-(defun v3bary (tri p)
+;; coeff of A is (1 - u - v). Will ne negative if 1 - ((u + v)/w) < 0 or
+;; fabs(u + v) > fabs(w).
+(defun bary (tri p)
   (let* ((a (v3->x tri))
 	 (b (v3->y tri))
 	 (c (v3->z tri))
@@ -88,23 +99,27 @@
 	 (ab (v2- b a))
 	 (ac (v2- c a))
 	 (orth (list ac ab pa))
-	 (crossv  (funcall #'v3x
+	 (crossv  (funcall #'v3cross
 			   (mapcar #'v2->x orth)
 			   (mapcar #'v2->y orth)))
 	 (u (v3->x crossv))
 	 (v (v3->y crossv))
+	 ;;  w = normalization constant.
 	 (w (v3->z crossv)))
     
-    (if (<  (abs w) 1)
+    (if (>  (abs (+ u v)) (abs w))
 	(v3 -1 1 1)
 	(v3 (- 1 (/ (+ u v) w))
 	    (/ v w)
 	    (/ u  w)))))
 
-(v3bary (list '(3 0 0) '(0 3 0) '(0 0 3)) '(3 0 0))
-(v3bary (list '(3 0 0) '(0 3 0) '(0 0 3)) '(0 3 0))
-(v3bary (list '(3 0 0) '(0 3 0) '(0 0 3)) '(0 0 3))
 
+(bary (list '(3 0) '(0 3) '(0 0)) '(3 0))
+(bary (list '(3 0) '(0 3) '(0 0)) '(0 3))
+(bary (list '(3 0) '(0 3) '(0 0)) '(0 0))
+(bary (list '(3 0) '(0 3) '(0 0)) '(4 0))
+(bary (list '(3 0) '(0 3) '(0 0)) '(0 4))
+(bary (list '(3 0) '(0 3) '(0 0)) '(4 4))
 
 ;; turn on a pixel
 (defun color-pixel (img p)
@@ -162,15 +177,39 @@
 	    (plot-line-high (v2->x p) (v2->y p) (v2->x q) (v2->y q))
 	    (plot-line-high (v2->x q) (v2->y q) (v2->x p) (v2->y p))))))
 
+;; returns pair of vec2 (minx, miny) (maxx, maxy)
+(defun aabb (pts)
+  (let
+      ((xs (mapcar #'v2->x pts))
+       (ys (mapcar #'v2->y pts)))
+    (list (v2 (apply #'min xs) (apply #'min ys))
+	  (v2 (apply #'max xs) (apply #'max ys)))))
+
+(aabb (list  (v2 0 0)
+	     (v2 0 1)
+	     (v2 1 0)
+	     (v2 1 1)))
+
+(defun plot-tri (tri)
+  (destructuring-bind (minv maxv) (aabb tri)
+    (loop for x from (v2->x minv) to (v2->x maxv)
+	  append (loop :for y from (v2->y minv) to (v2->y maxv)
+			:for p = (v2 x y)
+			:when (>= (apply #'min (bary tri p)) 0)
+			  collect p))))
+
+(plot-tri (v3 (v2 0 0) (v2 0 5) (v2 5 0)))
+
+
 (defun polar-project (c r theta)
   (v2floor (v2+ c (v2 (* r (cos theta))
 		      (* r (sin theta))))))
 
-;; entry point for chapter 1
 (defun ch2-main ()
   (let ((img (make-array (list +w+ +h+) :initial-element 0.5)))
-    (loop for theta from 0 to 6.2 by 0.1
-	  do (color-pixels img (plot-line +c+ (v2floor (polar-project +c+ +r+ theta)))))
+    (color-pixels img (plot-tri (v3 +c+
+				    (v2+ +c+ (v2 +r+ 0))
+				    (v2+ +c+ (v2 0 +r+)))))
     (write-img-to-file img "out-ch2.ppm")))
 
 
